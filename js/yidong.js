1c2aa06406d2bd07c8058a24eea9a63e3b478d81
baiguanghui <724618014@qqcom>
2018-05-29 21:09:50 +0800

    123

---
+++ js/yidong.js
@@ -0,0 +1,20 @@
+$(function(){
+
+    var mySwiper = new Swiper ('.swiper-container', {
+        direction: 'vertical',
+        loop: true,
+
+        // 如果需要分页器
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+
+    })
+
+
+
+
+
+
+})
