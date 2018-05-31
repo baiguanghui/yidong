5b477af60a02b0ac6cff05b194732bbdeeba1f83
baiguanghui <724618014@qq.com>
2018-05-31 18:49:55 +0800

    77777

--- js/yidong.js
+++ js/yidong.js
@@ -5,6 +5,8 @@ $(function(){
         initialSlide :4,
         slidesPerView : 1.5,
         centeredSlides : true,
+        effect : 'coverflow',
+
 
         // �����Ҫ��ҳ��
         pagination: {
